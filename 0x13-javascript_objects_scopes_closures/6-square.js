88d69e583255e830098c1c885b2959b3ad94eb79
Elizabeth Mashilo <98324930+LizzMash@users.noreply.github.com>
2022-08-03 09:22:58 +0200

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,29 @@
 #!/usr/bin/node
-module.exports = class Square extends require('./5-square.js') {
+const Rectangle = require('./5-square');
+module.exports = class Square extends Rectangle {
   charPrint (c) {
     if (c === undefined) {
-      this.print();
+      this.print(x);
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};#!/usr/bin/node
+const Rectangle = require('./5-square');
+module.exports = class Square extends Rectangle {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print(x);
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+    }
+  }
+};#!/usr/bin/node
+const Rectangle = require('./5-square');
+module.exports = class Square extends Rectangle {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print(x);
     } else {
       for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
